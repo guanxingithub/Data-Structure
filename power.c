b3b402329dd7c7d6b4fc32f686d38c4af4720a16
guanxingithub <38149783+guanxingithub@users.noreply.github.com>
2019-10-17 14:09:01 -0700

    Update power.c

--- power.c
+++ power.c
@@ -14,6 +14,17 @@ int pow2(int m, int n)
     return m * pow2(m*m, (n-1)/2);
   }
 }
+int pow3(int m, int n)
+{
+  if (n==0) return 1;
+  if (n%2 == 0) {
+    int r = pow3(m, n/2);
+    return r*r;
+  } else {
+    int r = pow3(m, (n-1)/2);
+    return m * r * r;
+  }
+}
 int main() 
 {
   int r;

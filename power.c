5b29863e0ec5026f702188e3a2b2cbbc0b237ab3
guanxingithub <38149783+guanxingithub@users.noreply.github.com>
2019-10-17 13:55:00 -0700

    Create power.c

---
+++ power.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int pow1(int m, int n)
+{
+  if (n == 0) return 1;
+  return pow1(m,n-1) * m;
+}
+int pow2(int m, int n)
+{
+  if (n== 0) return 1;
+  if ( n%2 == 0) {
+    return pow2(m*m, n/2);
+  } else {
+    return m * pow2(m*m, (n-1)/2);
+  }
+}
+int main() 
+{
+  int r;
+  r = pow1(2,9);
+  printf("%d ", r);
+  4 = pow2(2,9);
+  printf("%d ", r);
+  return 0;
+}
